9b2ae9edce1dc99b6254212ddac03fc8c7ff5b7c
scriperdj <scriperdj@gmail.com>
2016-01-15 19:09:32 +0530

    chnges

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,12 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
-
-
+require 'delayed/recipes'
 set :application, 'studionathan'
 set :repo_url, 'git@github.com:scriperdj/app_for_hari.git'
 set :branch, :master
 set :deploy_to, '/home/deploy/studionathan'
+
+set :delayed_job_command, "bin/delayed_job"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -62,5 +63,7 @@ namespace :deploy do
       # end
     end
   end
+
+  after "deploy:start", "delayed_job:start"
 
 end
